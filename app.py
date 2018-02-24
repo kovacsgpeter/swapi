f2253eeb3ad40984a0179eb87da9216779f69a84
Peter <kovacs.g.peter@gmail.com>
2018-02-24 18:07:54 +0100

    heroku5

--- app.py
+++ app.py
@@ -11,10 +11,7 @@ app.secret_key = 'whatever'
 
 
 
-def main():
-    app.run(debug=True,
-            host='0.0.0.0',
-            port=7000,)
+
 
 
 def get_api():
@@ -81,5 +78,3 @@ def logout():
     return redirect(url_for('route_main'))
 
 
-
-    main()
